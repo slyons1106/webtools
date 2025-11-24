66afd1551ccb68b23f9a81ca6dc3cd09a246f9d6
sean lyons <seanlyons@seans-MBP.localdomain>
2025-11-24 14:29:11 +0000

    feat(frontend): Refactor navigation menu and fix S3ViewerPage warning
    - Updated NavigationBar.tsx to:
      - Set 'WebTools' brand link to /person-details.
      - Add 'Lookup' menu item pointing to /person-details.
      - Remove duplicate 'Labels' menu item.
      - Remove redundant 'Person Details' menu item.
    - Fixed 'useEffect' is defined but never used warning in S3ViewerPage.tsx.

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -6,17 +6,17 @@ export function NavigationBar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/labels">WebTools</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/person-details">WebTools</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
+            <Nav.Link as={Link} to="/person-details">Lookup</Nav.Link>
             <Nav.Link as={Link} to="/labels">Labels</Nav.Link>
-            <Nav.Link as={Link} to="/person-details">Person Details</Nav.Link>
+
             <Nav.Link as={Link} to="/log-search">API Log Search</Nav.Link>
             <Nav.Link as={Link} to="/s3-viewer">S3 Label Viewer</Nav.Link>
             <Nav.Link as={Link} to="/csv-splitter">CSV Splitter</Nav.Link>
             <Nav.Link as={Link} to="/tools">Tools</Nav.Link>
-            <Nav.Link as={Link} to="/labels">Labels</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
